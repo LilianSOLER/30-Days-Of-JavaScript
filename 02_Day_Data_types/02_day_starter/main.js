3d2f30f1a7dd211edbd3e28f49e48a590f1b9d1a
SOLER Lilian <soler.lilian64@gmail.com>
2022-02-26 12:39:38 +0100

    Day 02 in progress - Ex1 q11-q19

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -20,3 +20,27 @@ console.log(`The rest is ${rest}`);
 let string = "Script";
 let includeString = challenge.includes(string);
 console.log(`This sentence include ${string} : ${includeString} `)
+
+let brands = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+let brandsArray = brands.split(', ');
+console.log(`Brands are : ${brands}\n
+Brands are : ${brandsArray[0]}, ${brandsArray[1]}, ${brandsArray[2]}, etc `);
+
+let challengePython = challenge.replace("JavaScript", "Python");
+console.log(challengePython);
+
+let letter = challenge.charAt(11);
+let letterCode = challenge.charCodeAt(11);
+console.log(`Unicode of ${letter} is ${letterCode}`);
+
+let searchLetter = "a";
+let firstIndexOfLetter = challenge.indexOf(searchLetter);
+let lastIndexOfLetter = challenge.lastIndexOf(searchLetter);
+console.log(`In "${challenge}" ${searchLetter} appear for the first time in ${firstIndexOfLetter + 1} place \
+and for the last time in ${lastIndexOfLetter + 1}`)
+
+let string2 = 'You cannot end a sentence with because because because is a conjunction';
+let searchString = "because"
+console.log(`In "${string2}" ${searchString} appear for the first time in ${string2.indexOf(searchString) + 1} place \
+and for the last time in ${string2.lastIndexOf(searchString) + 1}`);
+console.log(`Using search we find ${string2.search(searchString) +1}`);

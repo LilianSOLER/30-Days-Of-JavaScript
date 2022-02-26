40763354f74490cf701502ea3d1177b23ebc1eb1
SOLER Lilian <soler.lilian64@gmail.com>
2022-02-26 12:17:31 +0100

    Day 02 in progress - Learn primitive nd non-primitive data types, math object, random number, string and his methods, parse type to another - Ex1 q1-q10

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,1 +1,22 @@
 // this is your main.js script
+
+//EX1
+
+let challenge = '30 Days Of JavaScript';
+console.log(`${challenge} is ${challenge.length} long`);
+
+challengeUpper = challenge.toUpperCase();
+console.log(challengeUpper);
+challengeLower = challenge.toLowerCase();
+console.log(challengeLower);
+
+let challengeArray = challenge.split(" ");
+let firstWord = [0];
+console.log(`First Word is ${firstWord}`);
+
+let rest = challenge.substring(3);
+console.log(`The rest is ${rest}`);
+
+let string = "Script";
+let includeString = challenge.includes(string);
+console.log(`This sentence include ${string} : ${includeString} `)

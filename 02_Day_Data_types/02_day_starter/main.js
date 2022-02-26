ff60a1b233c95c684926ac747dcdc1026d813518
SOLER Lilian <soler.lilian64@gmail.com>
2022-02-26 15:10:00 +0100

    Day 02 in progress - Ex1 Finished

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -44,3 +44,19 @@ let searchString = "because"
 console.log(`In "${string2}" ${searchString} appear for the first time in ${string2.indexOf(searchString) + 1} place \
 and for the last time in ${string2.lastIndexOf(searchString) + 1}`);
 console.log(`Using search we find ${string2.search(searchString) +1}`);
+
+let string3 =  ' 30 Days Of JavaScript   ';
+let string3Trimed = string3.trim();
+let string3Array = string3Trimed.split(' ');
+let startString3 = string3Array[0];
+let endString3 = string3Array[string3Array.length-1];
+let aString3 = string3Trimed.match(/a/g);
+console.log(`-${string3}-\nbecome\n-${string3Trimed}-`);
+console.log(`${string3Trimed} start with ${startString3} is ${string3Trimed.startsWith(startString3)} and finish by ${endString3} is ${string3Trimed.endsWith(endString3)}`);
+console.log(aString3);
+
+let firstPartString3 = string3Trimed.substring(0, 7);
+let secondPartString3 = string3Trimed.substring(7);
+console.log(`${firstPartString3} + ${secondPartString3} = ${firstPartString3.concat(secondPartString3)}`);
+
+console.log(string3Trimed.repeat(2));

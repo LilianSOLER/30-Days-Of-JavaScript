133fea3f82702e834b9a1ad97dafe48d2fa0538d
SOLER Lilian <soler.lilian64@gmail.com>
2022-02-28 12:22:02 +0100

    Day 03 in progress - Learn True and False Value; Operators (assignment, comparision, increment, decrement, ternary); Window Methods (alert, prompt, confirm); Data Object(time Object) - EX1 q1-5

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,56 @@
 // this is your main.js script
+
+//EX1
+
+function typeOfArray(array){
+  if(array == null){
+    console.log("Array Null");  
+  }
+  else if(array.length == 0){
+    console.log("Array Empty");
+  }
+  else{
+    array.forEach(element => {
+      console.log(`${element} is ${typeof element}`);
+    });
+  }
+}
+
+function q1E1(){
+  let firstName, lastName, country, city, age, isMarried, year;
+  firstName = prompt("Enter your first name");
+  country = prompt("Enter your country");
+  city = prompt("Enter your city");
+  age = prompt("Enter your age");
+  isMarried = confirm("Are you married?");
+  year = new Date().getFullYear();
+
+  typeOfArray([firstName, country, city, age, isMarried, year]);
+  console.log(`${firstName} from ${country} in ${city} was born in ${year - age}`);
+  isMarried ? console.log("You are married") : console.log("You are not married");
+}
+
+//q1E1();
+
+console.log(`"10" == 10 is ${10 == "10"}`);
+console.log(`"10" === 10 is ${10 === "10"}`);
+console.log(`parseInt("9.8") == 10 is ${parseInt("9.8") == 10}`);
+console.log(`parseInt("9.8") === 10 is ${parseInt("9.8") === 10}`);
+
+function q4E1(){
+  let trueStatement = [true, 1, "random string"]; //might be true but random string isn't ??
+  let falseStatement = [false, 0, "", null, undefined, NaN]; //might be false but null, undefined, NaN aren't ??
+
+  trueStatement.forEach(element => {
+    console.log(`${element} is ${typeof element} is ${element == true}`);
+  });
+
+  falseStatement.forEach(element => {
+    console.log(`${element} is ${typeof element} is ${!(element == false)}`);
+  });
+}
+
+q4E1();
+
+
+

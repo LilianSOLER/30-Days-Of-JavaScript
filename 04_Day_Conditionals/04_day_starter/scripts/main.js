2fc78078e926bdf5a5d643f98b7bb3693497801b
SOLER Lilian <soler.lilian64@gmail.com>
2022-03-05 15:25:13 +0100

    Day 04 finished

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -134,75 +134,107 @@ function q2E1() {
 
 // q2E1();
 
-function q3E1(){
-  let a = parseInt(prompt("Enter a number"));
-  let b = parseInt(prompt("Enter a number"));
-  a < b ? console.log(`${a} is less than ${b}`) : console.log(`${a} is greater or equal than ${b}`);
+function q3E1() {
+	let a = parseInt(prompt("Enter a number"));
+	let b = parseInt(prompt("Enter a number"));
+	a < b
+		? console.log(`${a} is less than ${b}`)
+		: console.log(`${a} is greater or equal than ${b}`);
 }
 
 // q3E1();
 
-function q4E1(){
-  let a = parseInt(prompt("Enter a number"));
-  a % 2 == 0 ? console.log(`${a} is even`) : console.log(`${a} is odd`);
+function q4E1() {
+	let a = parseInt(prompt("Enter a number"));
+	a % 2 == 0 ? console.log(`${a} is even`) : console.log(`${a} is odd`);
 }
 
 // q4E1();
 
-function q1E2(){
-  let a = parseInt(prompt("Enter your scores"));
-  switch (true) {
-    case a >= 90: 
-      console.log("A");
-      break;
-    case a >= 70:
-      console.log("B");
-      break;
-    case a >= 60:
-      console.log("C");
-      break;
-    case a >= 50:
-      console.log("D"); 
-      break;
-    default:
-      console.log("F");
-  }
+function q1E2() {
+	let a = parseInt(prompt("Enter your scores"));
+	switch (true) {
+		case a >= 90:
+			console.log("A");
+			break;
+		case a >= 70:
+			console.log("B");
+			break;
+		case a >= 60:
+			console.log("C");
+			break;
+		case a >= 50:
+			console.log("D");
+			break;
+		default:
+			console.log("F");
+	}
 }
 
 // q1E2();
 
-function q2E2(){
-  let month = prompt("Enter a month").toLowerCase();
-  switch (month) {
-    case "january" || "february" || "deccember":
-      console.log("Winter");
-      break;
-    case "march" || "april" || "may":
-      console.log("Spring");
-      break;
-    case "june" || "july" || "august":
-      console.log("Summer");
-      break;
-    case "september" || "october" || "november":
-      console.log("Autumn");
-      break;
-    default:
-      console.log("Invalid month");
-  }
+function q2E2() {
+	let month = prompt("Enter a month").toLowerCase();
+	switch (month) {
+		case "january" || "february" || "december":
+			console.log("Winter");
+			break;
+		case "march" || "april" || "may":
+			console.log("Spring");
+			break;
+		case "june" || "july" || "august":
+			console.log("Summer");
+			break;
+		case "september" || "october" || "november":
+			console.log("Autumn");
+			break;
+		default:
+			console.log("Invalid month");
+	}
 }
 
 // q2E2();
 
-function q3E2(){
-  let day = prompt("Enter a day").toLowerCase();
-  if(day == "sunday" || day == "saturday"){
-    console.log("Weekend");
-  } else if(day == "monday" || day == "tuesday" || day == "wednesday" || day == "thursday" || day == "friday"){
-    console.log("Weekday");
-  } else {
-    console.log("Invalid day");
-  }
+function q3E2() {
+	let day = prompt("Enter a day").toLowerCase();
+	if (day == "sunday" || day == "saturday") {
+		console.log("Weekend");
+	} else if (
+		day == "monday" ||
+		day == "tuesday" ||
+		day == "wednesday" ||
+		day == "thursday" ||
+		day == "friday"
+	) {
+		console.log("Weekday");
+	} else {
+		console.log("Invalid day");
+	}
 }
 
-q3E2();
+// q3E2();
 
+function E3() {
+	let a = prompt("Enter a month").toLowerCase();
+	switch (a) {
+		case "january" ||
+			"march" ||
+			"may" ||
+			"july" ||
+			"august" ||
+			"october" ||
+			"december":
+			console.log("31 days");
+			break;
+		case "february":
+			console.log("28 days");
+			break;
+		case "april" || "june" || "september" || "november":
+			console.log("30 days");
+			break;
+		default:
+			console.log("Invalid month");
+	}
+}
+
+E3();
